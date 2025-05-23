94acf94844568f3990094ecbf190ce6ac6a9f750
zhonghong <hzhong@mcorp.ae>
2025-05-23 11:23:01 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 rm -rf ./public ./resources .hugo_build.lock
 git submodule update --init --recursive
 hugo
+# 部署到cloudflare后，需要更新algolia索引文件
+echo "\\n\033[31mTODO 部署到cloudflare后，需要更新algolia索引文件\033[0m"
