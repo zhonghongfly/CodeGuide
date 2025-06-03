c3c7c3b081313ba1557e7d36484e575165a8406a
zhonghong <hzhong@mcorp.ae>
2025-06-03 11:40:11 +0800

    谷歌广告

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 rm -rf ./public ./resources .hugo_build.lock
-git submodule update --init --recursive
+# git submodule update --init --recursive
 hugo
 # 部署到cloudflare后，需要更新algolia索引文件
 echo "\\n\033[31mTODO 部署到cloudflare后，需要更新algolia索引文件\033[0m"
