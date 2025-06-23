1088b0aa23318d6fe94f17bb182a65f6c40dbf82
zhonghong <31955384+zhonghongfly@users.noreply.github.com>
2025-06-23 10:46:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ git submodule update --init --recursive
 hugo
 # 部署到cloudflare后，需要更新algolia索引文件
 echo "\\n\033[31mTODO 部署到cloudflare后，需要更新algolia索引文件\033[0m"
+# 更新百度收录
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://zhonghongfly.com&token=FP68QzTpim52Q60x"
